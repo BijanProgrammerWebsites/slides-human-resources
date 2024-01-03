9e4e980288f06812003c9bb40c29ae953f8e09d7
BijanProgrammer <bijaneisapour@gmail.com>
2024-01-03 07:51:22 +0330

    [UPDATE] base styles

--- src/services/reveal.service.ts
+++ src/services/reveal.service.ts
@@ -3,22 +3,31 @@ import ZoomPlugin from 'reveal.js/plugin/zoom/zoom';
 
 export class RevealService {
     public start(): void {
-        const deck = new Reveal({
-            controlsLayout: 'edges',
-            slideNumber: 'c/t',
-            hashOneBasedIndex: true,
-            hash: true,
-            rtl: true,
-            display: 'grid',
-            hideCursorTime: 1000,
-            plugins: [ZoomPlugin],
-        });
+        const deck = new Reveal();
 
         // @ts-ignore
         deck.addKeyBinding('37', 'navigateLeft');
         // @ts-ignore
         deck.addKeyBinding('39', 'navigateRight');
 
-        deck.initialize();
+        deck.initialize({
+            controlsLayout: 'edges',
+            slideNumber: (slide): [string] => {
+                const horizontalOffset = slide && slide.dataset.visibility === 'uncounted' ? 0 : 1;
+
+                const currentSlideNumber = deck.getSlidePastCount(slide) + horizontalOffset;
+                const totalSlidesCount = deck.getTotalSlides();
+
+                const tokens = ['صفحه', currentSlideNumber, 'از', totalSlidesCount];
+                return [tokens.join(' ')];
+            },
+            hashOneBasedIndex: true,
+            hash: true,
+            rtl: true,
+            display: 'grid',
+            hideCursorTime: 1000,
+            plugins: [ZoomPlugin],
+            margin: 0.1,
+        });
     }
 }
